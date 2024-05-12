14aa9559bfe644d41a3e387d44e42783ab2d12f1
Hepiix <133647633+Hepiix@users.noreply.github.com>
2024-05-12 18:41:03 +0200

    .

---
+++ DiscordBot/Modules/RaiderIO/RaiderIOModule.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+using DiscordBot.Modules.RaiderIO.Models;
+
+namespace DiscordBot.Modules.RaiderIO
+{
+    public class RaiderIOModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        public RaiderIOService RaiderIOService { get; set; }
+
+        [SlashCommand("score", "Pokaż score danej postaci")]
+        public async Task ScoreSend([Name("Nazwapostaci")] [MinLength(2)]string charName, [Name("Nazwaserwera")] [MinLength(4)]string realmName)
+        {
+            realmName.Replace(' ', '-').ToLower();
+            CharacterRIO character = await RaiderIOService.GetData(charName, realmName);
+            if (character == null)
+            {
+                await RespondAsync("Spróbuj ponownie później!");
+            }
+            else
+            {
+                await RespondAsync($"{character.Name} - {character.MythicPlusScoresBySeason[0].Scores.All}");
+            }
+        }
+    }
+}
---
+++ DiscordBot/Modules/RaiderIO/RaiderIOService.cs
@@ -0,0 +1,37 @@
+﻿using DiscordBot.Modules.Tarot.Models;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using DiscordBot.Models;
+using DiscordBot.Services;
+using System.Threading.Channels;
+using DiscordBot.Modules.RaiderIO.Models;
+
+namespace DiscordBot.Modules.RaiderIO
+{
+    public class RaiderIOService : InteractionModuleBase<SocketInteractionContext>
+    {
+        HttpClient _httpClient = new HttpClient();
+        public async Task<CharacterRIO> GetData(string charName, string realmName)
+        {
+            string apiLink = $"https://raider.io/api/v1/characters/profile?region=eu&realm={realmName}&name={charName}&fields=guild%2Cgear%2Craid_progression%2Cmythic_plus_scores_by_season%3Acurrent";
+            HttpResponseMessage response = await _httpClient.GetAsync(apiLink);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                CharacterRIO character = JsonConvert.DeserializeObject<CharacterRIO>(responseBody);
+                GenerateImage(character);
+                return character;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void GenerateImage(CharacterRIO character)
+        {
+
+        }
+    }
+}
